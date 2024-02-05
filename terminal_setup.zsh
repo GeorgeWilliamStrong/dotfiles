a4b336062fc75b99322b1c98a35ca1bc74f850e9
georgewilliamstrong <geowstrong@gmail.com>
2024-02-05 15:55:26 +0000

    update

--- terminal_setup.zsh
+++ terminal_setup.zsh
@@ -6,6 +6,18 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/too
 
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 
+# Check if the .zshrc file exists
+if [[ -f "$zshrc_path" ]]; then
+    # Use sed to replace the content of line 18 with the new theme
+    sed -i '18s/.*/'"$new_theme"'/' "$zshrc_path"
+
+    echo "ZSH theme updated successfully in $zshrc_path"
+else
+    echo "Error: $zshrc_path not found."
+fi
+
+source ~/.zshrc
+
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
@@ -16,13 +28,12 @@ new_plugins='plugins=(git zsh-syntax-highlighting zsh-autosuggestions)'
 
 # Check if the .zshrc file exists
 if [[ -f "$zshrc_path" ]]; then
-    # Use sed to replace the content of line 18 with the new theme
-    sed -i '18s/.*/'"$new_theme"'/' "$zshrc_path"
-
     # Use sed to replace the content of line 80 with the new plugins
     sed -i '80s/.*/'"$new_plugins"'/' "$zshrc_path"
 
-    echo "ZSH theme and plugins updated successfully in $zshrc_path"
+    echo "ZSH plugins updated successfully in $zshrc_path"
 else
     echo "Error: $zshrc_path not found."
 fi
+
+source ~/.zshrc
